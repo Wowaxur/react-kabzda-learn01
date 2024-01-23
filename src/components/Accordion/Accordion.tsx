22c4dc4992593bd04ecb317934db06771e57c2ea
Wowaxur <wowaxur@yandex.ru>
2024-01-23 15:48:24 +0300

    code review

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,33 +1,46 @@
+import React from "react";
+
+type ItemType = {
+    title : string
+    value: any
+}
     type AccordionPropsType = {
-        titleValue: string;
+        titleValue: string
         collapsed: boolean
         onChange: () => void
+        items: ItemType[]
+        onClick: (value:any) => void
+
     }
+
 
     function Accordion(props: AccordionPropsType) {
         return (
             <div>
-                <AccordionTitle title={props.titleValue} onChange={props.onChange}/>
-                {!props.collapsed && <AccordionBody/>}
+                <AccordionTitle title={props.titleValue}
+                                onChange={props.onChange}/>
+                {!props.collapsed && <AccordionBody onClick={props.onClick} items = {props.items}/>}
             </div>
         )
     }
-
+    type AccordionBodyPropsType = {
+        items: ItemType[]
+        onClick: (value:any) => void
+    }
     type AccordionTitlePropsType = {
         title: string
         onChange: () => void
 
     }
 
+
     function AccordionTitle(props: AccordionTitlePropsType) {
         return <h3 onClick={props.onChange}>---{props.title}---</h3>
     }
 
-    function AccordionBody() {
+    function AccordionBody(props: AccordionBodyPropsType) {
         return <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            {props.items.map((item, index) => <li onClick={()=>{props.onClick(item.value)}} key={index}>{item.title}</li>)}
         </ul>
     }
 
