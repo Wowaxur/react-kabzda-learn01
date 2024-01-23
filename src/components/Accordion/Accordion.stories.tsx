cbaceacba03b2b918f57fbad6dd551ce4ba92a0f
Wowaxur <wowaxur@yandex.ru>
2024-01-23 15:47:35 +0300

    create accordion stories

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -6,9 +6,34 @@ export default {
     component: Accordion
 }
 
-export const CollapsedAccordion = () => <Accordion titleValue={'ACCORDION'} collapsed={true} onChange={()=>{}}/>
-export const UncollapsedAccordion = () => <Accordion titleValue={'ACCORDION'} collapsed={false} onChange={()=>{}}/>
+export const CollapsedAccordion = () => <Accordion onClick={a => a}
+                                                   items={[]}
+                                                   titleValue={'ACCORDION'}
+                                                   collapsed={true}
+                                                   onChange={() => {
+                                                   }}/>
+export const UncollapsedAccordion = () => <Accordion titleValue={'ACCORDION'}
+                                                     collapsed={false}
+                                                     onClick={a => a}
+                                                     onChange={() => {}}
+                                                     items={[
+                                                         {title: 'vld', value: '1'},
+                                                         {title: 'gdw', value: '2'},
+                                                         {title: 'wow', value: '3'},
+                                                     ]}/>
 export const ChangebleAccordion = () => {
     const [collapsed, setCollapsed] = useState(true)
-     return <Accordion titleValue={'ACCORDION'} collapsed={collapsed}  onChange={()=>{setCollapsed(!collapsed)}}/>
+
+    return <Accordion
+        titleValue={'ACCORDION'}
+        collapsed={collapsed}
+        onClick={a => a}
+        onChange={() => {
+            setCollapsed(!collapsed)
+        } }
+        items={[
+            {title: 'vld', value: '1'},
+            {title: 'gdw', value: '2'},
+            {title: 'wow', value: '3'},
+        ]}/>
 }
